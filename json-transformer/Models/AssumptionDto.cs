f3f3734ebc50e87bcf68bf2ab12b7dc1e09801ec
holmen1 <holmen1@gmail.com>
2025-03-22 17:24:05 +0100

    Initial commit

---
+++ json-transformer/Models/AssumptionDto.cs
@@ -0,0 +1,14 @@
+namespace json_transformer.Models;
+
+public class AssumptionDto
+{
+    public string Level1 { get; set; }
+    public string Level2 { get; set; }
+    public string Level3 { get; set; }
+    public double Value { get; set; }
+}
+
+public class AssumptionsModelDto
+{
+    public List<AssumptionDto> Assumptions { get; set; }
+}
---
+++ json-transformer/Program.cs
@@ -0,0 +1,36 @@
+using json_transformer.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapPost("/assumptions", (AssumptionsModelDto model) =>
+    {
+        // Process the assumptions data (add your logic here)
+        // For now, just return the received data as confirmation
+        return Results.Ok(model);
+    })
+    .WithName("PostAssumptions")
+    .WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
